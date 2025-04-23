9dc4d4274a3a02547c456720e9ed054e121dab4d
eceransurya <99064079+eceransurya@users.noreply.github.com>
2025-04-23 07:14:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'hello world';
+});
+
+Route::get('/world', function () {
+    return 'world';
+});
+
+Route::get('/about', function () {
+    return 'Nim : 23552021032 <br> Nama : Ahmad Hamdan Habibulloh'  ;
+    
+});
+
+Route::get('mahasiswa', function ($id) {
+});
+Route::put('mahasiswa', function ($id) {
+});
+Route::delete('mahasiswa', function ($id) {
+});
+Route::get('mahasiswa/{id}', function ($id) {
+});
+Route::put('mahasiswa/{id}', function ($id) {
+});
+Route::delete('mahasiswa{id}', function ($id) {
+});
